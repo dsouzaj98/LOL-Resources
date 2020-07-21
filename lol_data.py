9d1aef832870581f7322a907ee7a04de7c42e388
dsouzaj98 <66758205+dsouzaj98@users.noreply.github.com>
2020-07-21 19:22:40 -0400

    update graphs

--- lol_data.py
+++ lol_data.py
@@ -3,6 +3,8 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import scipy.stats as stats
+import math
+import wordninja
 
 
 #reads dataset in from kaggle into pandas
@@ -27,7 +29,6 @@ blue_data=lol_data.drop(['gameId','blueWins','redWardsPlaced', 'redWardsDestroye
 #creates Dataframe of pvalues and t statistics
 tstats=[]
 pvals=[]
-print(len(red_data.columns))
 for idx in range(len(red_data.columns)):
     tstat, pval=stats.ttest_ind(red_data.iloc[:,idx], blue_data.iloc[:,idx])
     tstats.append(tstat)
@@ -38,6 +39,22 @@ for col in red_data.columns:
     new_names.append(col[3:])
    
 df=pd.DataFrame(list(zip(tstats, pvals)), index=new_names, columns=["T-stat", "P-value"])
+
+#graphs normal distributions
+for idx in range(len(red_data.columns)):
+    fig1, ax1=plt.subplots(1,1)
+    red_dist=stats.norm(red_data.iloc[:,idx].mean(), red_data.iloc[:,idx].std())
+    blue_dist=stats.norm(blue_data.iloc[:,idx].mean(), blue_data.iloc[:,idx].std())
+    x_range=np.linspace(red_dist.ppf(0.01), red_dist.ppf(0.99),101)
+    ax1.plot(red_dist.pdf(x_range), color="red")
+    ax1.plot(blue_dist.pdf(x_range), color="blue")
+    ax1.set_title(label=' '.join(wordninja.split(red_data.columns[idx][3:])))
+    ax1.set_xlabel("Number of samples")
+    ax1.set_ylabel(f" {' '.join(wordninja.split(red_data.columns[idx][3:]))}")
+    ax1.axvline(red_dist.mean(), color="red", linestyle=':')
+    ax1.axvline(blue_dist.mean(), color="blue", linestyle=':')
+    plt.show()
+    
 
 
 
